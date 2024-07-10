4fee13473b683d6268de907b14086a7e9d2d6618
Ipek Canli <119710326+i99c@users.noreply.github.com>
2024-07-11 00:55:41 +0300

    image sorunu çözüldü

--- GossipTime/GossipTime/settings.py
+++ GossipTime/GossipTime/settings.py
@@ -137,4 +137,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'media'
