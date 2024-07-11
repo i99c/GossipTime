2c39ef5b83d495f2d3348ec80903d55a35e2fd0f
Ipek Canli <119710326+i99c@users.noreply.github.com>
2024-07-11 17:06:17 +0300

    sadece models yapısına like ve dislike eklendi düzenlenmedi

--- GossipTime/Writer/models.py
+++ GossipTime/Writer/models.py
@@ -16,6 +16,47 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='images/', null=True, blank=True)
+    is_delete = models.BooleanField(verbose_name=('Silindi?'), default=False)
+    delete_date = models.DateTimeField(verbose_name=('Silinme Tarihi'), blank=True, null=True) 
 
     def __str__(self):
         return self.title
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, verbose_name=('Post'), on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, verbose_name=('Kullanıcı'), on_delete=models.CASCADE)
+    create_date = models.DateTimeField(verbose_name=('Oluşturma Tarihi'), auto_now_add=True)
+    update_date = models.DateTimeField(verbose_name=('Son güncelleme tarihi'), auto_now=True)
+    is_delete = models.BooleanField(verbose_name=('Silindi?'), default=False)
+    delete_date = models.DateTimeField(verbose_name=('Silinme Tarihi'), blank=True, null=True) 
+
+    class Meta:
+        verbose_name = 'Beğeni'
+        verbose_name_plural = 'Beğeniler'
+        ordering = ['-created-date']
+
+    def __str__(self):
+        if self.user.first_name and self.user.last_name:
+            return f"{self.user.first_name} {self.user.last_name} - {self.blog.title}"
+        else:
+            return f"{self.user.username} - {self.blog.title}"
+
+class Dislike(models.Model):
+    post = models.ForeignKey(Post, verbose_name=('Post'), on_delete=models.CASCADE, related_name='dislikes')
+    user = models.ForeignKey(User, verbose_name=('Kullanıcı'), on_delete=models.CASCADE)
+    create_date = models.DateTimeField(verbose_name=('Oluşturma Tarihi'), auto_now_add=True)
+    update_date = models.DateTimeField(verbose_name=('Son güncelleme tarihi'), auto_now=True)
+    is_delete = models.BooleanField(verbose_name=('Silindi?'), default=False)
+    delete_date = models.DateTimeField(verbose_name=('Silinme Tarihi'), blank=True, null=True) 
+
+    class Meta:
+        verbose_name = 'Beğenmeme'
+        verbose_name_plural = 'Beğenmemeler'
+        ordering = ['-created-date']
+
+    def __str__(self):
+        if self.user.first_name and self.user.last_name:
+            return f"{self.user.first_name} {self.user.last_name} - {self.blog.title}"
+        else:
+            return f"{self.user.username} - {self.blog.title}"
